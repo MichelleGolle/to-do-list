5dd1a528cc12e3ba1078208e61d3d4e1ddff7642
Michelle Golle <michelleg54@gmail.com>
2015-08-07 18:25:14 -0600

    added twitter login and jquery search box

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
 root to: "home#index"
 
-resources :tags, only: [:index, :new, :create] 
+resources :tags, only: [:index, :new, :create]
 resources :users
 resources :lists do
   resources :tasks
@@ -15,5 +15,7 @@ get "archived", to:  "lists#archived_index"
 get "login", to:     "sessions#new"
 post "login", to:    "sessions#create"
 delete "logout", to: "sessions#destroy"
+
+get 'auth/:provider/callback', to: 'sessions#create'
 
 end
